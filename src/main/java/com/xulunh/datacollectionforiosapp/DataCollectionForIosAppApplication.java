96a66d9fbc47746bbbc92eb60501fc072855c6d1
Xulun Huang <jasonh1@bu.edu>
2025-09-04 20:05:00 -0400

    proj initiated

---
+++ src/main/java/com/xulunh/datacollectionforiosapp/DataCollectionForIosAppApplication.java
@@ -0,0 +1,13 @@
+package com.xulunh.datacollectionforiosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectionForIosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCollectionForIosAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xulunh/datacollectionforiosapp/DataCollectionForIosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xulunh.datacollectionforiosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionForIosAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
